024abf7fd2f01eae5297bd06c29581c5ad8717b7
NTimofeev <nt-90@yandex.ru>
2024-07-03 21:50:10 +0300

    feat: simple-cors

--- app.js
+++ app.js
@@ -12,11 +12,21 @@ const errorHandler = require('./middlewares/error-handler');
 
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
+const allowedCors = ['localhost:3000'];
+
 mongoose.connect(MONGO_URL, {
   family: 4,
 });
 
 const app = express();
+
+app.use((req, res, next) => {
+  const { origin } = req.headers;
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+  next();
+});
 
 app.use(requestLogger);
 
