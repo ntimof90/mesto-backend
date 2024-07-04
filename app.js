d70d678d8430de6e007d3af36f5f9b49ff2787da
NTimofeev <nt-90@yandex.ru>
2024-07-04 21:07:00 +0300

    fix cors

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const mongoose = require('mongoose');
 
 const { errors } = require('celebrate');
 
+const cors = require('cors');
+
 const router = require('./routes');
 
 const { MONGO_URL } = require('./config');
@@ -20,26 +22,7 @@ mongoose.connect(MONGO_URL, {
 
 const app = express();
 
-app.use((req, res, next) => {
-  const { origin } = req.headers;
-  if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
-  }
-
-  const { method } = req; // Сохраняем тип запроса (HTTP-метод) в соответствующую переменную
-
-  // Значение для заголовка Access-Control-Allow-Methods по умолчанию (разрешены все типы запросов)
-  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
-  // Если это предварительный запрос, добавляем нужные заголовки
-  if (method === 'OPTIONS') {
-    const requestHeaders = req.headers['Access-Control-Request-Headers'];
-    // разрешаем кросс-доменные запросы любых типов (по умолчанию)
-    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-    res.header('Access-Control-Allow-Headers', requestHeaders);
-    res.end();
-  }
-  next();
-});
+app.use(cors({ origin: allowedCors }));
 
 app.use(requestLogger);
 
